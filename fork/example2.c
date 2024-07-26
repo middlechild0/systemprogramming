1efac647ba05a5148d6666930d6577bf78e2d1b1
middlechild0 <jimmymathu8@gmail.com>
2024-07-26 12:17:46 +0300

    fork changes

--- fork/example2.c
+++ fork/example2.c
@@ -1,38 +1,26 @@
 #include <unistd.h>
 #include <stdio.h>
 
-int main() 
-{
-    // Creating the first child
+int main() {
     int n1 = fork();
-
-    // Creating the second child. The first child
-    // also executes this line and creates a grandchild.
     int n2 = fork();
 
-    if (n1 > 0 && n2 > 0) 
-    {
+    if (n1 > 0 && n2 > 0) {
         printf("parent\n");
         printf("n1 = %d, n2 = %d\n", n1, n2);
         printf("My ID is %d\n", getpid());
         printf("My parent ID is %d\n", getppid());
-    } 
-    else if (n1 == 0 && n2 > 0) 
-    {
+    } else if (n1 == 0 && n2 > 0) {
         printf("First child\n");
         printf("n1 = %d, n2 = %d\n", n1, n2);
         printf("My ID is %d\n", getpid());
         printf("My parent ID is %d\n", getppid());
-    } 
-    else if (n1 > 0 && n2 == 0) 
-    {
+    } else if (n1 > 0 && n2 == 0) {
         printf("Second child\n");
         printf("n1 = %d, n2 = %d\n", n1, n2);
         printf("My ID is %d\n", getpid());
         printf("My parent ID is %d\n", getppid());
-    } 
-    else 
-    {
+    } else {
         printf("Third child\n");
         printf("n1 = %d, n2 = %d\n", n1, n2);
         printf("My ID is %d\n", getpid());
